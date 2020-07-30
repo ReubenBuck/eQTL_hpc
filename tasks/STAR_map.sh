d1a2994d11ad57625be9f4d7ba1c7107d1b0cfe5
Buckley <reuben.buckley@nih.gov>
2020-07-30 13:13:49 -0400

    star_test

--- tasks/STAR_map.sh
+++ tasks/STAR_map.sh
@@ -3,34 +3,36 @@
 
 # for read groups we can create a manifest
 
+# invoke with: sbatch --cpus-per-task=12 --mem=35g --gres=lscratch:20 STAR_map.sh
+
+CONFIG=/home/buckleyrm/data/projects/eQTL/config_files/AUSS_25461.config
+SM=$(cat $CONFIG | cut -f1 | tail -n 1)
+RUNDIR=/home/buckleyrm/data/projects/eQTL/run_dir/$SM
+mkdir -p $RUNDIR
+
+INDEX=/home/buckleyrm/data/resources/genomes/CanFam3.1/STAR
+
+TMPDIR=/lscratch/$SLURM_JOB_ID/STARtmp
 
 
-CONFIG=/Users/buckleyrm/Desktop/eQTL_metadata/sample_rna_seq_runs.txt
-GENOMEDIR=/Users/buckleyrm/Desktop/RNA_data_test/test_run/in/genome/CanFam3.1/fasta
-GENOME=CanFam3.1.fa
-GTFDIR=/Users/buckleyrm/Desktop/RNA_data_test/test_run/in/genome/CanFam3.1/gtf
-GTF=CanFam3.1_NCBI.gtf
-BAMDIR=/Users/buckleyrm/Desktop/RNA_data_test/test_run/out/bam
-TMPDIR=/Users/buckleyrm/Desktop/RNA_data_test/test_run/out/tmp
-SM=S1
 
 
-awk '{print $9"\t"$10"\tID:"$1"."$3"."$4"."$5"\\tPU:"$4"."$5"."$6"\\tSM:"$1"\\tPL:"$2"\\tLB:"$3}' $CONFIG | tail -n +2 > $TMPDIR/$SM.manifest.txt
+awk '{print $9"\t"$10"\tID:"$1"."$3"."$4"."$5"\\tPU:"$4"."$5"."$6"\\tSM:"$1"\\tPL:"$2"\\tLB:"$3}' $CONFIG | tail -n +2 > $RUNDIR/manifest.txt
 
 
-#module load samtools/1.6 || fail "could not load samtools module"
-#module load STAR         || fail "could not load STAR module"
+module load samtools/1.9 || fail "could not load samtools module"
+module load STAR/2.7.3a || fail "could not load STAR module"
 
 
 STAR \
-    --runThreadN 1 \
-    --genomeDir $GENOMEDIR/$GENOME \
-    --sjdbOverhang 50 \
-    --readFilesManifest $TMPDIR/$SM.manifest.txt \
+    --runThreadN $SLURM_CPUS_PER_TASK \
+    --genomeDir $INDEX \
+    --sjdbOverhang 75 \
+    --readFilesManifest $RUNDIR/manifest.txt \
     --readFilesCommand zcat \
     --outSAMtype BAM SortedByCoordinate \
-    --outTmpDir=/$TMPDIR \
-    --outFileNamePrefix $BAMDIR/$SM
+    --outTmpDir=$TMPDIR \
+    --outFileNamePrefix $RUNDIR/$SM
 
 
 
